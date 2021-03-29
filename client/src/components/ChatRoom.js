f68d8835ae01852a3ff213283dea0d06baeb0963
dchicchon <danielchicchon@gmail.com>
2021-03-29 12:28:15 -0700

    change server in client

--- client/src/components/ChatRoom.js
+++ client/src/components/ChatRoom.js
@@ -12,9 +12,9 @@ import {
 } from "@material-ui/core";
 
 const ChatRoom = () => {
-  // const SERVER = "https://chat-app-server-chicchon.herokuapp.com/"; /// deployment
+  const SERVER = "https://chat-app-server-chicchon.herokuapp.com/"; /// deployment
   // const SERVER = "http://10.0.0.119:5000"; get this based on computer working on
-  const SERVER = "http://10.0.0.37:5000";
+  // const SERVER = "http://10.0.0.37:5000";
   const [socket, setSocket] = useState(null);
   const [loading, setLoading] = useState(true);
   const [newMessage, setNewMessage] = useState("");
