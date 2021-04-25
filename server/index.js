f4d8340dce00392af9f7f9a36ed056478b6620f2
dchicchon <danielchicchon@gmail.com>
2021-04-25 16:36:09 -0700

    update server code to create table if it doesnt exist

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require("express")();
-const { Pool } = require("pg")
+const { Pool } = require("pg");
 const http = require("http").Server(app);
 const PORT = process.env.PORT || 5000;
 const io = require("socket.io")(http, {
@@ -7,8 +7,9 @@ const io = require("socket.io")(http, {
 });
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://me:password@localhost:5432/api',
-})
+  connectionString:
+    process.env.DATABASE_URL || "postgres://me:password@localhost:5432/api",
+});
 
 // pool.connect()
 let users = 0;
@@ -18,41 +19,58 @@ app.get("/", (req, res) => {
 
 // io is server, socket is each user?
 const getMessages = async () => {
-  const { rows } = await pool.query("SELECT * FROM messages")
-  return rows
-}
+  console.log("Getting messages");
+  const { rows } = await pool.query("SELECT * FROM messages");
+  return rows;
+};
 
 io.on("connection", (socket) => {
   users++;
   console.log("A user connected");
-  let user = socket.handshake.query.user
-  let newDate = new Date().toDateString()
-  pool.query('INSERT INTO messages (text, sender, date) VALUES ($1, $2, $3)', ["Welcome to the server " + user, 'ChatBot', newDate], (error, res) => {
-    if (error) {
-      throw error;
+  let user = socket.handshake.query.user;
+  let newDate = new Date().toDateString();
+  // console.log(pool);
+  pool.query(
+    "CREATE TABLE IF NOT EXISTS messages (id serial PRIMARY KEY, text VARCHAR(255) NOT NULL, sender VARCHAR(255) NOT NULL, date VARCHAR(255) NOT NULL );"
+  );
+  pool.query(
+    "INSERT INTO messages (text, sender, date) VALUES ($1, $2, $3)",
+    ["Welcome to the server " + user, "ChatBot", newDate],
+    (error, res) => {
+      if (error) {
+        throw error;
+      }
+      getMessages().then((messages) => {
+        io.emit("new-user", { users, messages });
+      });
     }
-    getMessages().then((messages) => {
-      io.emit("new-user", { users, messages })
-    })
-  })
+  );
 
   // On receiving a message
   socket.on("send-message", (data) => {
-    // console.log(data);
-    pool.query('INSERT INTO messages (text, sender, date) VALUES ($1, $2, $3)', [data.text, data.sender, data.date], (error, res) => {
-      getMessages().then(messages => {
-        io.emit("receiving-message", messages);
-      })
-    })
+    console.log(data);
+    pool.query(
+      "INSERT INTO messages (text, sender, date) VALUES ($1, $2, $3)",
+      [data.text, data.sender, data.date],
+      (error, res) => {
+        getMessages().then((messages) => {
+          io.emit("receiving-message", messages);
+        });
+      }
+    );
   });
   socket.on("disconnect", (socket) => {
     console.log("A user disconnected");
     users--;
-    pool.query('INSERT INTO messages (text, sender, date) VALUES ($1, $2, $3)', [user + "Has left the chat", "ChatBot", new Date().toDateString()], (error, res) => {
-      getMessages().then(messages => {
-        io.emit("user-disconnected", { users, messages });
-      })
-    })
+    pool.query(
+      "INSERT INTO messages (text, sender, date) VALUES ($1, $2, $3)",
+      [user + " has left the chat", "ChatBot", new Date().toDateString()],
+      (error, res) => {
+        getMessages().then((messages) => {
+          io.emit("user-disconnected", { users, messages });
+        });
+      }
+    );
   });
 });
 
